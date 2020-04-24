5879e0fec8173d2ac048112b09e16e8c755e4876
Latosha Tims <ltims080913@hotmail.com>
2020-04-24 09:46:49 -0700

    hit snag with auth token. continued to setting up PrivateRoute. Rendered BubblePage to App. Made GET request in BubblePage. Completed DELETE request function to delete colors.

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -8,9 +8,20 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
-
+  useEffect(() => {
+    axiosWithAuth()
+    .get('/api/colors')
+    .then(res => {
+      setColorList(res.data)
+      console.log(colorList)
+    })
+    .catch(error => {
+      console.log('POP goes your bubble', error)
+    })
+  }, [])
   return (
     <>
+      <h1>This is the Bubble Page</h1>
       <ColorList colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
     </>
